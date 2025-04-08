41bf7a942151f604fcae816babd6c15925ddabee
Lucas Camargo <lucas@camargo.eng.br>
2025-04-08 10:09:20 +0200

    [input] more progress

--- src/input/input.cpp
+++ src/input/input.cpp
@@ -63,6 +63,9 @@ bool input::init(ryml::ConstNodeRef cfg)
         setup_default_actions();
     }
 
+    SDL_SetGamepadEventsEnabled(true);
+    log::info("[input] enabled gamepads");
+
     return true;
 }
 
@@ -97,6 +100,8 @@ bool input::event(SDL_Event *evt)
                 log::info("[input] gp pressed: %d", (int) btn);
             }
         }
+        else
+            log::warn("[input] evt without gp: %d", joy_id);
     }
     else if(evt->type == SDL_EVENT_GAMEPAD_BUTTON_UP)
     {
@@ -115,6 +120,16 @@ bool input::event(SDL_Event *evt)
     else if(evt->type == SDL_EVENT_GAMEPAD_AXIS_MOTION)
     {
         // TODO buffer input
+    }
+    else if(evt->type == SDL_EVENT_KEY_DOWN)
+    {
+        // TODO buffer input
+        log::verb("[input] key down: %d", (int)evt->key.scancode);
+    }
+    else if(evt->type == SDL_EVENT_KEY_UP)
+    {
+        // TODO buffer input
+        log::verb("[input] key up: %d", (int)evt->key.scancode);
     }
 
     return true;
@@ -147,11 +162,14 @@ void input::gamepad_add(uint32_t joy_id)
             .has_gyro = SDL_GamepadHasSensor(gp, SDL_SENSOR_GYRO),
             .has_accel = SDL_GamepadHasSensor(gp, SDL_SENSOR_ACCEL)
         };
-        log::info("[input] gamepad_add: registered: %d%s%s%s", 
+        auto name = SDL_GetGamepadName(gp);
+        log::info("[input] gamepad_add: registered: '%s' (%d)%s%s%s %d", 
+            name? name : "",
             joy_id, 
             _d->gps_data[joy_id].has_rumble? " rumble":"",
             _d->gps_data[joy_id].has_gyro? " gyro":"",
             _d->gps_data[joy_id].has_accel? " accel":"");
+            SDL_SetGamepadPlayerIndex(gp, 1);
     }
     else
         log::warn("[input] gamepad_add: cannot open: %d: %s", joy_id, SDL_GetError());
@@ -162,13 +180,13 @@ void input::gamepad_remove(uint32_t joy_id)
     auto it = _d->gamepads.find(joy_id);
     if(it == _d->gamepads.end())
     {
-        log::warn("[input] gamepad_remove: not registered: %d");
+        log::warn("[input] gamepad_remove: not registered: %d", joy_id);
         return;
     }
     SDL_CloseGamepad(it->second);
     _d->gps_data.erase(it->first);
     _d->gamepads.erase(it);
-    log::info("[input] gamepad_remove: removed %d");
+    log::info("[input] gamepad_remove: removed %d", joy_id);
 }
 
 void input::setup_default_actions()
