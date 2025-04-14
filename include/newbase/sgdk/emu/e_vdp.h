8844bdac3b35bde2099796bd1733768f53a0e785
Lucas Camargo <lucas@camargo.eng.br>
2025-04-14 15:24:20 +0200

    [sgdk] some headers

---
+++ include/newbase/sgdk/emu/e_vdp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+namespace nb {
+
+class sgdk_vdp final {
+public:
+    sgdk_vdp();
+    ~sgdk_vdp();
+
+    // renders a frame
+    void render_frame();
+
+    std::vector<uint8_t>& vram() {return m_vram;}
+    std::vector<uint16_t>& cram() {return m_cram;}
+    std::vector<uint16_t>& vsram() {return m_vsram;}
+
+    // memory sizes
+    static constexpr size_t VRAM_SZ = 64*1024; // consider increasing this if widescreen (in the future)
+    static constexpr size_t CRAM_SZ = 64; // use u16 for color (9-bit rgb fits nicely)
+    static constexpr size_t VSRAM_SZ = 2*25; // internal cram stores r,g,b values separately
+
+    static constexpr size_t TILE_SZ = 32; // tile size in bytes
+    static constexpr size_t SPRITE_SZ = 8; // sprite def size in bytes
+
+private:
+    std::vector<uint8_t> m_vram;
+    std::vector<uint16_t> m_cram;
+    std::vector<uint16_t> m_vsram;
+};
+
+}
---
+++ include/newbase/sgdk/sgdk.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <newbase/system.h>
+
+namespace nb {
+
+// this system provides a C API for compatibility with SGDK games
+// it needs to:
+// - implement an emulated VDP
+// - implement emulated audio chips (YM2612 and PSG)
+// - expose input as the C API expects it
+// - any other support systems
+// - does not have to be fully complete, games will inform what API is needed
+// - games must use the native newbase APIs when appropriate, macros are ok
+class sgdk : public system
+{
+public:
+    sgdk();
+    ~sgdk();
+
+    SDL_InitFlags sdl_subsystems(ryml::ConstNodeRef cfg) override {return 0;}
+    entt::id_type metatype_id() override { return entt::hashed_string{"sgdk"}.value(); }
+    //bool can_bind() override { return true; }
+    //void bind(void *state) override;
+
+    bool init(ryml::ConstNodeRef cfg) override;
+    bool step(step_phase) override;
+    bool event(SDL_Event*) override;
+
+private:
+    sgdk_p *_d;
+};
+
+}
