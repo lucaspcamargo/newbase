8da1b22e156e29e5e82e5015ac0ddf30bdf226e8
Lucas Camargo <lucas@camargo.eng.br>
2025-10-18 14:25:38 +0200

    [demo] adjust screen boundaries

--- demo/res/asteroids/stage.lua
+++ demo/res/asteroids/stage.lua
@@ -1,7 +1,7 @@
 -- stage script
 
 physics2d_set_gravity(vec2.new(0.0, 0.0));
-render_cam_2d_setup(0, 0, 1024, 1024);
+render_cam_2d_setup(0, 0, 1920, 1080);
 
 if system_audio then
     audio_bgm_play(hs("res/asteroids/bgm/ObservingTheStar/ObservingTheStar.ogg"));
