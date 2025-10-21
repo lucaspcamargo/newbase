65d3ac7b9fa4970c97762fe569c3d87cd0d54a59
Lucas Camargo <lucas@camargo.eng.br>
2025-10-21 18:27:25 +0200

    [glm] utility header

---
+++ include/newbase/utility/glm.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+// GLM - OpenGL Mathematics
+// This header not only includes the GLM functionality we use,
+// but also defines some template specializations to allow usage
+// with some standard library constructs used for bindings,
+// such as std::is_nothrow_copy_constructible, std::is_nothrow_copy_assignable, etc.
+
+// if not using these constructs with GLM types, nothing stops us from including GLM directly :)
+
+#include <glm/glm.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <type_traits>
+
+// std type_traits specializations
+
+namespace std {
+    template<>
+    struct is_nothrow_copy_constructible<glm::vec2> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_constructible<glm::vec3> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_constructible<glm::vec4> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_constructible<glm::mat4> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_constructible<glm::quat> : std::true_type {};
+
+    template<>
+    struct is_nothrow_copy_assignable<glm::vec2> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_assignable<glm::vec3> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_assignable<glm::vec4> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_assignable<glm::mat4> : std::true_type {};
+    template<>
+    struct is_nothrow_copy_assignable<glm::quat> : std::true_type {};
+}
